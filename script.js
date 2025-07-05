c2e5710433b8ba9fd82b7c6a56dd9f4bb0867e6a
birdycz <tranbaophann@gmail.com>
2025-07-05 13:52:44 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function openLetter() {
+  document.getElementById("letterPopup").classList.remove("hidden");
+}
+function closeLetter() {
+  document.getElementById("letterPopup").classList.add("hidden");
+}
