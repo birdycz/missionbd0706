5730290ac35de4b5915448ffcfa39639918061c5
birdycz <tranbaophann@gmail.com>
2025-07-05 14:09:37 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function openLetter() {
   document.getElementById("letterPopup").classList.remove("hidden");
 }
+
 function closeLetter() {
   document.getElementById("letterPopup").classList.add("hidden");
 }
